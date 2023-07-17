d5af65153c997a077aa62e44bded42bc5953193a
roni10morello <roman@kogreen.ru>
2023-07-18 00:49:07 +0300

    1-4.1

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
