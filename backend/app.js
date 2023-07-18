cf8886967d925c21576c1c051eae6a01cd43323c
Roman Abramovich <roman@kogreen.ru>
2023-07-19 02:43:35 +0300

    1-8

--- backend/app.js
+++ backend/app.js
@@ -15,12 +15,6 @@ const auth = require('./middlewares/auth');
 const app = express();
 
 const { PORT = 3000 } = process.env;
-// app.use(cors({
-//   origin: 'http://localhost:3000',
-//   credentials: true,
-// }));
-
-//app.use(cors());
 
 app.use(cors({
   origin: 'https://morello.nomoreparties.sbs',
